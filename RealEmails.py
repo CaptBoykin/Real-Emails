a24ac97e9d6945e4b8eb732c9d39ff812681ca77
planetxort <39013067+planetxort@users.noreply.github.com>
2020-06-29 23:47:52 -0600

    Create RealEmails.py

---
+++ RealEmails.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import re
+
+#What's really on your mind? Each word is replaced by the corresponding string
+#For example, hope will get replaced with:"genuinely do not care if"
+fixit = {'hope':'genuinely do not care if','did you':'did you fucking','Per my last email,':'Did you fucking read my last email','take your time':'please, can you hurry the fuck up','I\'m not in a rush':'We are literally waiting on you'}
+def replfunc(match):
+   return fixit[match.group(0)]
+
+#Slaps everything together for sendin'
+#Where email.txt is your original file and email-fix.txt will be created
+regex = re.compile('|'.join(re.escape(x) for x in fixit))
+with open('email.txt') as fin, open('email-fixed.txt','w') as fout:
+    for line in fin:
+        fout.write(regex.sub(replfunc,line))
