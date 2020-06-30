e3a87f7f91c5eb03ba4c0154fcf1e7de14c09887
Tyler Boykin <tyler.boykin@bylight.com>
2020-06-30 06:58:45 +0000

    Appended phrases to RealEmails.py

--- RealEmails.py
+++ RealEmails.py
@@ -4,7 +4,23 @@ import re
 
 #What's really on your mind? Each word is replaced by the corresponding string
 #For example, hope will get replaced with:"genuinely do not care if"
-fixit = {'hope':'genuinely do not care if','did you':'did you fucking','Per my last email,':'Did you fucking read my last email','take your time':'please, can you hurry the fuck up','I\'m not in a rush':'We are literally waiting on you','Respectfully':'Take a long walk off a short pier'}
+fixit = {
+	'hope':'genuinely do not care if',
+	'did you':'did you fucking',
+	'Per my last email,':'Did you fucking read my last email',
+	'take your time':'please, can you hurry the fuck up',
+	'I\'m not in a rush':'We are literally waiting on you',
+	'Respectfully':'Take a long walk off a short pier',
+	'not up to standards':'an ass sandwich',
+	'sub par':'butt fudge',
+	'Sincerely':'Choke on my balls',
+	'sounds good':'sounds god awful',
+	'I can\'t wait!':'I\'d rather nail my dick to a table',
+	'no rush':'hurry the fuck up',
+	'pushing to the right':'delaying forever',
+	'pushed to the right':'delayed forever',
+	'meeting':'torture-fest'
+}
 def replfunc(match):
    return fixit[match.group(0)]
 
