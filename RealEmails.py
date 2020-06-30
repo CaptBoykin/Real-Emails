975ae8553d963f74ed579574f2dce2c5c7241ccf
pr0digal <pr0digal@users.noreply.github.com>
2020-06-30 02:12:06 -0400

    Making Respectfully a little less respectful

--- RealEmails.py
+++ RealEmails.py
@@ -4,7 +4,7 @@ import re
 
 #What's really on your mind? Each word is replaced by the corresponding string
 #For example, hope will get replaced with:"genuinely do not care if"
-fixit = {'hope':'genuinely do not care if','did you':'did you fucking','Per my last email,':'Did you fucking read my last email','take your time':'please, can you hurry the fuck up','I\'m not in a rush':'We are literally waiting on you'}
+fixit = {'hope':'genuinely do not care if','did you':'did you fucking','Per my last email,':'Did you fucking read my last email','take your time':'please, can you hurry the fuck up','I\'m not in a rush':'We are literally waiting on you','Respectfully':'Take a long walk off a short pier'}
 def replfunc(match):
    return fixit[match.group(0)]
 
